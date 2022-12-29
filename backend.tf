6427bc2e670ca037f0db52090853b9e05a34b7ba
cyrilleaws <88727676+cyrilleaws@users.noreply.github.com>
2022-12-29 13:38:12 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "cyrille1-bucket-tf"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "cyrille1-dynamo-db-table"
   }
 }
